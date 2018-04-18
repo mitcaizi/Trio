a72b22cf99d99ab1b17e8cfb86e4a669281ae8f5
mitcaizi <jling1@mail.csuchico.edu>
2018-04-17 22:25:56 -0700

    new

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
+void swap (int &x, int &y);
+void swap (int &x, int &y)
+{
+int temp;
+temp = x;
+x = y;
+y=temp;
+}
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -8,10 +15,18 @@ int main()
   cout<<"Enter Red, Green, and Blue values: ";
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
-
-
-
-
+if (red < green)
+{
+swap (red, green);
+}
+if (red < blue)
+{
+swap (red, blue);
+}
+if (green < blue)
+{
+swap (green,blue);
+}
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
